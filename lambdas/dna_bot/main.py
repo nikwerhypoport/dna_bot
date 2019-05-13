40c4cf89eea9fbfc61eb1eddf7b1b5f0c4f02aef
nikolaus.werner <nikolaus.werner@hypoport.de>
2019-05-13 12:00:49 +0200

    minimal request security and request validation

--- lambdas/dna_bot/main.py
+++ lambdas/dna_bot/main.py
@@ -1,21 +1,51 @@
 import json
+import os
+from typing import Dict
+from hmac import compare_digest, new
+import hashlib
 
 
-def is_valid(signature):
-    # validate against secret
-    return True
+def _response(status:int, message:str):
+    return {
+        'isBase64Encoded': False,
+        'headers': {},
+        'statusCode': status,
+        'body': '{"message" : "%s" }' % message.replace('"', '\\"').replace('\n', '\\n')  # json escape
+    }
 
 
-def branch_creation_handler(event, context):
+def _error(status=400, message='Could not perform request'):
+    return _response(status, message)
+
+
+def _ok(message: str):
+    return _response(200, message)
+
+
+def _is_valid(request_signature: str, payload: str):
+    secret = os.environ['SECRET']
+    signature = f'sha1={new(key=secret.encode(), msg=payload, digestmod=hashlib.sha1)}'
+    return compare_digest(signature, request_signature)
+
+
+def branch_creation_handler(event: Dict, context):
 
     content = event.get('body')
-    print(content)
+    if not content:
+        return _error(400, 'payload empty or missing')
 
-    signature = event.get('headers',{}).get('X-Hub-Signature')
-    print(signature)
+    body_as_str = json.dumps(content, indent=None)
+    if not body_as_str:
+        return _error(400, 'payload empty or missing')
 
-    if is_valid(signature):
-        print('ok')
+    signature = event.get('headers', {}).get('X-Hub-Signature')
+    if not signature:
+        return _error(400, 'X-Hub-Signature missing')
+
+    if not _is_valid(signature, body_as_str):
+        return _error(403, 'X-Hub-Signature is invalid')
+
+    return _ok('alles klar')
 
     # print(event)
     #print(json.dumps(event, indent=False))
