a1ebf7e1746aa694ab9f56ceb5cb4eb8241a6c9a
nikolaus.werner <nikolaus.werner@hypoport.de>
2019-05-14 11:08:37 +0200

    linked comment when new branch is created

--- lambdas/dna_bot/main.py
+++ lambdas/dna_bot/main.py
@@ -1,20 +1,21 @@
 import json
 import os
 from typing import Dict
-from github import Github, GithubException, GitRef
+from github import Github, GithubException
 
 from dna_bot.commons import _is_valid, _error, _ok, GithubEventType
 
 
-def _is_issue_comment(event: Dict):
-    return GithubEventType.ISSUE_COMMENT.value == event.get('headers', {}).get('X-GitHub-Event')
+def _is_issue_comment_created(event: Dict):
+    return GithubEventType.ISSUE_COMMENT.value == event.get('headers', {}).get('X-GitHub-Event') and \
+            event.get('body', {}).get('action') == 'created'
 
 
 def _contains_command(content: str):
     return content.find('_cb_') >= 0
 
 
-def _create_branch(content: Dict) -> GitRef:
+def _create_branch(content: Dict) -> str:
     issue_number: str = content.get('issue', {}).get('number')
     repository: str = content.get('repository', {}).get('name')
     branch_counter = 0
@@ -25,13 +26,13 @@ def _create_branch(content: Dict) -> GitRef:
 
     while True:
         try:
-            ref = g.get_organization('hypoport').get_repo(repository).create_git_ref(f'refs/heads/{branch_name}', head)
+            g.get_organization('hypoport').get_repo(repository).create_git_ref(f'refs/heads/{branch_name}', head)
         except GithubException as e:
             branch_name, branch_counter = _rename_if_exists(e, issue_number, branch_counter)
         else:
             break
 
-    return ref
+    return branch_name
 
 
 def _rename_if_exists(e, issue_number, branch_counter):
@@ -44,11 +45,18 @@ def _rename_if_exists(e, issue_number, branch_counter):
         raise e
 
 
-def _create_comment(content: Dict, branch_url: str):
+def _update_comment(content: Dict, branch_name: str):
+    repo_url = content.get('repository', {}).get('html_url')
+    branch_html_url = f'{repo_url}/tree/{branch_name}'
     issue_number: str = int(content.get('issue', {}).get('number'))
     repository: str = content.get('repository', {}).get('name')
     g = Github(os.environ['GITHUB_ACCESS_TOKEN'])
-    g.get_organization('hypoport').get_repo(repository).get_issue(issue_number).create_comment(f'Created [branch]({branch_url})')
+    #g.get_organization('hypoport').get_repo(repository).get_issue(issue_number).create_comment(f'Created [branch]({branch_html_url})')
+
+    comment_id = int(content.get('comment', {}).get('id'))
+    comment = g.get_organization('hypoport').get_repo(repository).get_issue(issue_number).get_comment(comment_id)
+    comment.edit(f'{comment.body} -> Created [branch]({branch_html_url})')
+    comment.update()
 
 
 def branch_creation_handler(event: Dict, context):
@@ -68,17 +76,16 @@ def branch_creation_handler(event: Dict, context):
     if not _is_valid(signature, body_as_str):
         return _error(403, 'X-Hub-Signature is invalid')
 
-    if not _is_issue_comment(event):
+    if not _is_issue_comment_created(event):
         return _ok('Nothing to do -event is not an issue comment')
 
     comment_body = content.get('comment', {}).get('body')
     if not comment_body:
-        _ok(f'No comment content found')
+        return _ok(f'No comment content found')
 
     if not _contains_command(comment_body):
-        _ok(f'No magic command in comment')
+        return _ok(f'No magic command in comment')
 
-    # return _ok('Nothing to do - event is not an issue comment')
-    ref = _create_branch(content)
-    _create_comment(content, ref.url)
-    return _ok(f'created branch {ref.ref} on {content.get("repository", {}).get("html_url")}')
+    branch_name = _create_branch(content)
+    _update_comment(content, branch_name)
+    return _ok(f'created branch {branch_name} on {content.get("repository", {}).get("html_url")}')
